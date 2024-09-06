b490f67729a035470fcafd61d54dc8bb2954daa3
najwakussyafira <you@kussyafira@gmail.com>
2024-09-06 09:47:57 +0700

    commitPraktikum01

---
+++ Praktikum 01/Latihanjava.java
@@ -0,0 +1,12 @@
+/**
+ * Latihan14
+ */
+public class Latihanjava {
+    public static void main(String[] args) {
+        System.out.println("Jurusan: teknologi informasi");
+        System.out.println("saya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+        System.out.print("Nama: Najwa Kus Safira");
+        System.out.print("Prodi:D4 SIB");
+    
+    }
+}
