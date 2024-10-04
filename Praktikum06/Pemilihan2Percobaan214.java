662910ef4daa700634082d0e4edc2c4cbac7afb3
najwakussyafira <you@kussyafira@gmail.com>
2024-10-04 09:53:31 +0700

    Praktikum06

--- Praktikum06/Pemilihan2Percobaan214.java
+++ Praktikum06/Pemilihan2Percobaan214.java
@@ -3,8 +3,6 @@ package Praktikum06;
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan214 {
-
-    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -24,56 +22,56 @@ public class Pemilihan2Percobaan214 {
         sc.nextLine();
         System.out.println("Apakah punya member (y/n) ? =");
         member = sc.nextLine();
-        System.out.println("-------------------------------------"); 
-    if (member.equalsIgnoreCase("y")) {
-        diskon = 0.10;
-        System.out.println("Besar diskon = 10%");
-        if (pilihan_menu == 1) {
-            harga = 14000;
-            System.out.println("Harga ricebowl = " + harga);
+        System.out.println("-------------------------------------");
 
-        } else if (pilihan_menu == 2) {
-            harga = 3000;
-            System.out.println("Harga ice tea = " + harga);
+        if (member.equalsIgnoreCase("y")) {
+            diskon = 0.10;
+            System.out.println("Besar diskon = 10%");
+            if (pilihan_menu == 1) {
+                harga = 14000;
+                System.out.println("Harga ricebowl = " + harga);
 
-        } else if (pilihan_menu == 3) {
-            harga = 15000;
-            System.out.println("Harga bundling = " + harga);
+            } else if (pilihan_menu == 2) {
+                harga = 3000;
+                System.out.println("Harga ice tea = " + harga);
+
+            } else if (pilihan_menu == 3) {
+                harga = 15000;
+                System.out.println("Harga bundling = " + harga);
+
+            } else {
+                System.out.println("Masukkan pilihan menu dengan benar");
+                return;
+
+            }
+
+            total_bayar = harga - (harga * diskon);
+            System.out.println("Total bayar setelah diskon = " + total_bayar);
+        } else if (member.equalsIgnoreCase("n")) {
+            if (pilihan_menu == 1) {
+                harga = 14000;
+                System.out.println("Harga ricebowl = " + harga);
+
+            } else if (pilihan_menu == 2) {
+                harga = 3000;
+                System.out.println("Harga ice tea = " + harga);
+
+            } else if (pilihan_menu == 3) {
+                harga = 15000;
+                System.out.println("Harga bundling = " + harga);
+
+            } else {
+                System.out.println("Masukkan pilihan menu dengan benar");
+                return;
+            }
+
+            System.out.println("Total bayar = " + harga);
 
         } else {
-            System.out.println("Masukkan pilihan menu dengan benar");
-            return; 
-            
+            System.out.println("Member tidak valid");
         }
+        System.out.println("------------------------------");
 
-        total_bayar = harga - (harga * diskon);
-        System.out.println("Total bayar setelah diskon = " + total_bayar);
-    }      
-    else if (member.equalsIgnoreCase("n")) {
-        if  (pilihan_menu == 1) {
-            harga = 14000;
-            System.out.println("Harga ricebowl = " + harga);
-
-        } else if (pilihan_menu == 2) {
-            harga = 3000;
-            System.out.println("Harga ice tea = " + harga);
-
-        } else if (pilihan_menu == 3) {
-            harga = 15000;
-            System.out.println("Harga bundling = " + harga);
-
-        } else {
-            System.out.println("Masukkan pilihan menu dengan benar");
-            return;
-        }
-
-        System.out.println("Total bayar = " + harga);
-
-    }  else {
-        System.out.println("Member tidak valid");
-    } 
-    System.out.println("------------------------------");
-    
-    sc.close();
-}
+        sc.close();
+    }
 }
